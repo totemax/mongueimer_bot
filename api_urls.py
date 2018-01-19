5351f7a31394f72a764119e08ceb3704835c000d
jjorge <jjorgeserran@gmail.com>
2018-01-19 20:47:39 +0100

    remove api urls useless file

--- api_urls.py
+++
@@ -1,2 +0,0 @@
-player_summary = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=%s&steamids=%d&format=json'
-app_data = 'http://store.steampowered.com/api/appdetails?appids=%d'
